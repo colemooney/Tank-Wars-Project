16091ccc6996cc7f2b1738fb5fc877449102ad00
Cole Mooney <colemooney@Coles-MacBook-Pro.local>
2019-09-04 16:35:25 -0400

    refactoring

--- index.js
+++ index.js
@@ -25,37 +25,43 @@ var ctx = canvas.getContext('2d');
     ctx.fillRect(0, 500 , 600, 100)
     ctx.fill()
 }
-var tank = {
-    x: 300,
-    y: 500,
-     moveLeft:  function() { this.x -= 25 },
-     moveRight: function() { this.x += 25 },
-  }
-  
-  function drawEnemy(enemy){
 
   function getRandomInt(max) {
-        return Math.floor(Math.random() * Math.floor(max));
+         return Math.floor(Math.random() * Math.floor(max));
+      
       }
+  function drawEnemy(enemy){
+
+  
+      
     var canvas = document.getElementById('game');
      var ctx = canvas.getContext('2d');
   
      enemy = new Image();
      
-     
+    //  let getRandomInt(500) = point
 
-    var xE = getRandomInt(550)
     
-    var yE = 50 
+    var zzzz= Math.floor(Math.random() * Math.floor(50))
+    setInterval(enemy.onload = function() {
+        var xE = zzzz;
     
-    enemy.onload = function() {
-          
-        ctx.drawImage(enemy, xE, yE , 50, 50);
-      
+    
+        var yE = 50;
+        updateCanvas()
+        let xR = setInterval(xE , 5000)
+        let yF = setInterval(yE + Math.floor(Math.random() * Math.floor(50)), 100);
+        
+        // debugger
+        zzzz+=Math.floor(Math.random() * Math.floor(50))
+        ctx.clearRect(enemy, xE, yE, 50, 50);
+        ctx.drawImage(enemy, xR , yF , 50, 50);
+        // ctx.drawImage(enemy, xE , yF , 50, 50);
+        
        
-        drawEnemy()
-         ctx.clearRect(enemy, xE, yE, 50, 50)
-      }
+        // drawEnemy()
+        //  ctx.clearRect(enemy, xE, yE, 50, 50)
+      }, 50)
     
     enemy.src = './images/enemy.png';
   }
@@ -64,7 +70,12 @@ var tank = {
 //     let yM;
     
 //   }
-  
+var tank = {
+    x: 300,
+    y: 500,
+     moveLeft:  function() { this.x -= 25 },
+     moveRight: function() { this.x += 25 },
+  }
   function drawTank(tank){
   
   
@@ -76,14 +87,31 @@ var tank = {
   
     img.onload = function() {
           ctx.drawImage(img, tank.x, tank.y,100,100);
-      console.log(tank)
-      console.log(img)
-     
+      
       };
   
     img.src = './images/tank.png';
   }
-  
+  setInterval(function shot(){
+    var canvas = document.getElementById('game');
+    var ctx = canvas.getContext('2d');
+      let xS;
+      var yS = tank.y;
+      let yS = setInterval(490 - Math.floor(Math.random() * Math.floor(50)), 100);
+      function shooting(){
+        // let yS = yS - 10;
+        updateCanvas()
+        
+        
+        
+        ctx.clearRect(xS, yS, 5, 20)
+        ctx.fillStyle= "yellow"
+        ctx.fillRect(xS, yS, 5, 20)
+        ctx.fill()
+        
+      }
+      console.log(shooting())
+  }, 100)
 //   function enemies(){
 //     var canvas = document.getElementById('game');
 //     var ctx = canvas.getContext('2d');
@@ -126,6 +154,8 @@ var tank = {
 
 
     document.onkeydown = function(e) {
+        var canvas = document.getElementById('game');
+     var ctx = canvas.getContext('2d');
       
       switch (e.keyCode) {
         case 37: tank.moveLeft();
@@ -134,38 +164,42 @@ var tank = {
         case 39: tank.moveRight(); 
         console.log('right', tank); 
             break;
-        case 38: shoot(); 
-        console.log('shoot', tank); 
+        case 38: shot(); 
+        console.log('shoot'); 
             break;
       }
       
+      
     }
   
-// function updateEnemy() {
-//     console.log("woor")
-//     var canvas = document.getElementById('game');
-//      var ctx = canvas.getContext('2d');
-//      ctx.clearRect(0,0,1500,1700);
-//     drawBack() 
-//     spawn()
-    
-//   }
+
   
   
-  function updateCanvas(tank) {
+  function updateCanvas() {
     var canvas = document.getElementById('game');
      var ctx = canvas.getContext('2d');
     ctx.clearRect(0,0,1500,1700);
-    drawBack() 
-    drawEnemy()
     drawTank(tank)
+    
+    drawBack() 
+    
    } 
- 
+   function updateEnemy() {
+    console.log("woor")
+    var canvas = document.getElementById('game');
+     var ctx = canvas.getContext('2d');
+     
+     updateCanvas()
+
+    drawEnemy()
+    
+  }
   
 
 document.getElementById("logo").onclick = function startGame(){
     console.log("starting game");
     document.getElementById('game');
-    updateCanvas(tank)
- 
+    
+    updateEnemy()
+    // setInterval(startGame()), 10)
 }   
